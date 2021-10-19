df54d424275069c176e7e8d1e19d294276b75fe5
schmit62u <schmit62u@etu.univ-lorraine.fr>
2021-10-19 16:52:27 +0200

    Generated libgdx project

---
+++ project_labyrinth/desktop/src/com/project/labyrinth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project.labyrinth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project.labyrinth.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
