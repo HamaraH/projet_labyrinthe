5b7b4a26397ccf92fa772a37261f158e40d399c5
Arno RAMM <Arno.Ramm@telecomnancy.eu>
2021-10-27 17:55:11 +0200

    player & map sprite

--- core/src/com/project/labyrinth/Game.java
+++ core/src/com/project/labyrinth/Game.java
@@ -1,31 +1,46 @@
 package com.project.labyrinth;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	SpriteBatch map_batch;
+	Texture map_texture;
+	SpriteBatch player_batch;
+	Texture player_texture;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// creating the map
+		map_batch = new SpriteBatch();
+		map_texture= new Texture("grass.png");
+
+		//creating the player object
+
+		player_batch = new SpriteBatch();
+		player_texture= new Texture("player_standing.png");
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		map_batch.begin();
+		map_batch.draw(map_texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		map_batch.end();
+
+		player_batch.begin();
+		player_batch.draw(player_texture, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 50, 50);
+		player_batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		map_batch.dispose();
+		map_texture.dispose();
 	}
 }
