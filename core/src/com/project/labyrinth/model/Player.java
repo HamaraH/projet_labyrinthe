cb3023fdd7902841b9238af699fd66b8cbd31cf3
lindsay <lindsay.noyer6@etu.univ-lorraine.fr>
2021-10-27 16:29:59 +0200

    function move in player

--- core/src/com/project/labyrinth/model/Player.java
+++ core/src/com/project/labyrinth/model/Player.java
@@ -1,4 +1,19 @@
 package com.project.labyrinth.model;
 
 public class Player extends Entity {
+
+
+    public Player(int x, int y){
+        this.posX = x;
+        this.posY = y;
+
+    }
+
+    public void move(int x,int y){
+
+        this.posX += x;
+        this.posY += y;
+    }
+
+
 }
